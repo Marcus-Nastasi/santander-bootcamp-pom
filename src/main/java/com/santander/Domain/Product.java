69e8a1f9a298fb39be6a9a077addc033235cb478
rolemberg <vinnie.nsts@gmail.com>
2024-05-14 18:44:44 -0300

    created Product class for testing;

---
+++ src/main/java/com/santander/Domain/Product.java
@@ -0,0 +1,28 @@
+package com.santander.Domain;
+
+public class Product {
+
+    private final long id;
+    private final String name;
+    private double price;
+
+    public Product(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+
