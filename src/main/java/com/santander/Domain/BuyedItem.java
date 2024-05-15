87310fc29895d32739d1b8f5e4200033bdcdc771
rolemberg <vinnie.nsts@gmail.com>
2024-05-15 18:29:33 -0300

    creating update method on BuyedItem;

--- src/main/java/com/santander/Domain/BuyedItem.java
+++ src/main/java/com/santander/Domain/BuyedItem.java
@@ -19,12 +19,25 @@ public class BuyedItem {
         return("BuyedItem{" + "product=" + product + ", quantity=" + quantity + '}');
     }
 
+    public void update(BuyedItem buyedItem) {
+        this.setProduct(buyedItem.getProduct());
+        this.setQuantity(buyedItem.getQuantity());
+    }
+
     public Product getProduct() {
         return product;
     }
 
     public int getQuantity() {
         return quantity;
+    }
+
+    private void setProduct(Product product) {
+        this.product = product;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
 
--- src/main/java/com/santander/Domain/Purchase.java
+++ src/main/java/com/santander/Domain/Purchase.java
@@ -21,6 +21,12 @@ public class Purchase {
         this.buyedItems.remove(buyedItem);
     }
 
+    public void updateBuyedItem(BuyedItem buyedItem1, BuyedItem buyedItem2) {
+        this.buyedItems.forEach(b -> {
+            if (b.equals(buyedItem1)) b.setProduct(buyedItem2.getProduct());
+        });
+    }
+
     @Override
     public String toString() {
         return("Purchase{" + "buyedItems=" + buyedItems + ", subTotal: $" + this.calculateTotal() + '}');
