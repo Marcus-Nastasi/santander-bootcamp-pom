8fadbbcf9b1f91f132cf7b1c20728230e160bb78
rolemberg <vinnie.nsts@gmail.com>
2024-05-14 23:05:14 -0300

    testing Purchase methods;

--- src/main/java/com/santander/Main.java
+++ src/main/java/com/santander/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
         Product product = new Product(1001, "Apple", 1.99);
         BuyedItem buyedItem = new BuyedItem(product, 2);
-
+        Purchase purchase = new Purchase();
+        purchase.addBuyedItem(buyedItem);
+        System.out.println(purchase.calculateTotal());
     }
 }
 
