5aa8362eb108cd338f60929489aac18fd5d1296b
rolemberg <vinnie.nsts@gmail.com>
2024-05-15 17:33:05 -0300

    testing on Main;

--- src/main/java/com/santander/Main.java
+++ src/main/java/com/santander/Main.java
@@ -7,6 +7,9 @@ import com.santander.Domain.Purchase;
 public class Main {
     public static void main(String[] args) {
         Product product = new Product(1001, "Apple", 1.99);
+        Product product2 = new Product(1002, "Banana", 0.99);
+        Product product3 = new Product(1003, "Orange", 2.99);
+        Product product4 = new Product(1004, "Torange", 4.99);
         BuyedItem buyedItem = new BuyedItem(product, 2);
         Purchase purchase = new Purchase();
         purchase.addBuyedItem(buyedItem);
