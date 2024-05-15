bf3901b0c800ef4e1246009ec5f82a7df36b588a
rolemberg <vinnie.nsts@gmail.com>
2024-05-14 22:41:55 -0300

    created Purchase methods;

--- src/main/java/com/santander/Domain/Purchase.java
+++ src/main/java/com/santander/Domain/Purchase.java
@@ -9,6 +9,19 @@ public class Purchase {
 
     public Purchase() {}
 
+    public void addBuyedItem(BuyedItem buyedItem) {
+        this.buyedItems.add(buyedItem);
+    }
+
+    public void removeBuyedItem(BuyedItem buyedItem) {
+        this.buyedItems.remove(buyedItem);
+    }
+
+    @Override
+    public String toString() {
+        return("Purchase{" + "buyedItems=" + buyedItems + '}');
+    }
+
     public List<BuyedItem> getBuyedItems() {
         return buyedItems;
     }
